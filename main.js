c63edc24100f6a44fbc42a8467a759cd6dc1f2cf
andriTinas <andrianustinas.siskom@gmail.com>
2019-09-11 12:08:39 +0700

    03. Miner rewards & transactions - Blockchain in Javascript (part 3)

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const SHA256 = require('crypto-js/sha256');
 
+class Transaction{
+    constructor(fromAddress,toAddress,amount){
+        this.fromAddress    = fromAddress;
+        this.toAddress      = toAddress;
+        this.amount         = amount;
+    }
+}
+
 class Block{
-    constructor(index,timestamp,data,previousHash=''){
-        this.index          = index;
+    constructor(timestamp,transaction,previousHash=''){
         this.timestamp      = timestamp;
-        this.data           = data;
+        this.transaction    = transaction;
         this.previousHash   = previousHash;
         this.hash           = '';
         this.nonce          = 0;
@@ -27,20 +34,53 @@ class Block{
 class BlockChain{
     constructor(){
         this.chain=[this.createGenesisBlock()];
-        this.difficulty=4;
+        this.difficulty=2;
+        this.pendingTransactions= [];
+        this.miningReward       = 100;
     }
     createGenesisBlock(){
-        return new Block(0,"01/08/2019","Genesis Blok","0");
+        return new Block("01/08/2019","Genesis Blok","0");
     }
     getLatestBlock(){
         return this.chain[this.chain.length -1];
     }
-    addBlock(newBlock){
-        newBlock.previousHash   = this.getLatestBlock().hash;
-        // newBlock.hash           = newBlock.calculateHash();
-        newBlock.mainBlock(this.difficulty);
-        this.chain.push(newBlock);
+    // addBlock(newBlock){
+    //     newBlock.previousHash   = this.getLatestBlock().hash;
+    //     newBlock.mainBlock(this.difficulty);
+    //     this.chain.push(newBlock);
+    // }
+    minePendingTransaction(miningRewardAddress){
+        let block = new Block(Date.now(),this.pendingTransactions);
+        block.mainBlock(this.difficulty);
+        
+        console.log("Block successfully mined!");
+        this.chain.push(block);
+
+        this.pendingTransactions =[
+            new Transaction(null,miningRewardAddress,this.miningReward)
+        ];
+        // console.log("pendingTransactions \n" , this.pendingTransactions);
     }
+
+    createTransaction(transaction){
+        this.pendingTransactions.push(transaction);
+    }
+
+    getBalanceOfAddress(address){
+        let balance = 0;
+        for(const block of this.chain){
+            for(const trans of block.transaction){
+                if(trans.fromAddress === address){
+                    balance -= trans.amount;
+                }
+                if(trans.toAddress === address){
+                    balance += trans.amount;
+                }
+            }
+        }
+        return balance;
+    }
+
     isChainValid(){
         for(let i = 1; i < this.chain.length;i++){
             const currentBlock  = this.chain[i];
@@ -58,10 +98,29 @@ class BlockChain{
 
 let CAI = new BlockChain();
 
-console.log("Mining blok 1.... ");
-CAI.addBlock(new Block(1, "05/08/2019",{amout:4}));
+CAI.createTransaction(new Transaction('address1','address2',100));
+CAI.createTransaction(new Transaction('address2','address1',25));
 
-console.log("Mining blok 2.... ");
-CAI.addBlock(new Block(2, "08/08/2019",{amout:10}));
+console.log("\nStarting the minier.....");
+CAI.minePendingTransaction('TinDev-address');
+console.log("\nBalance of TinDev is",CAI.getBalanceOfAddress('TinDev-address'));
 
+console.log("\nStarting the minier again.....");
+CAI.minePendingTransaction('TinDev-address');
+console.log("\nBalance of TinDev is",CAI.getBalanceOfAddress('TinDev-address'));
 
+console.log("\nBalance of address1 is",CAI.getBalanceOfAddress('address1'));
+console.log("\nBalance of address2 is",CAI.getBalanceOfAddress('address2'));
+console.log("\n");
+
+CAI.minePendingTransaction('address2');
+CAI.minePendingTransaction('address2');
+CAI.minePendingTransaction('address2');
+console.log("\nBalance of address2 is",CAI.getBalanceOfAddress('address2'));
+
+console.log("\n");
+
+CAI.minePendingTransaction('address1');
+CAI.minePendingTransaction('address1');
+CAI.minePendingTransaction('address1');
+console.log("\nBalance of address1 is",CAI.getBalanceOfAddress('address1'));
